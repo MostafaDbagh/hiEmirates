c6d2b65e2dfa48d3685b03a4b9fb183b9631daf5
MostafaDbagh <dabagmostafa@gmail.com>
2021-10-30 13:31:38 -0700

    make it better

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,5 +1,5 @@
 import {Column,Header,Logo,Phone,Mail,Divbutton,Button } from '../style/headerstyle'
-import Navbar from './Navbar'
+import Navbar from './navbar'
 import {useState} from 'react'
 
 
