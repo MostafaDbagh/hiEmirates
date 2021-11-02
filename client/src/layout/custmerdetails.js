9a063209012dc8869d042959ad5bb47417d3d0d3
MostafaDbagh <dabagmostafa@gmail.com>
2021-11-02 12:09:29 -0700

    make it better

--- client/src/layout/custmerdetails.js
+++ client/src/layout/custmerdetails.js
@@ -2,32 +2,23 @@ import React, { useState } from 'react';
 
 
 const Customerdetails  = () => {
-  let[customer,setCustomer]=useState({
-    Username:"",
-    Email:"",
-    Phone:"",
-    address:"",
-    Bpassport:"",
-    Fpassport:"",
-    Personalimage:""
-  })
+  let[customer,setCustomer]=useState({ })
   const handleoneuser= (n)=> {
     fetch(`https://hellouae.herokuapp.com/getuser/${n}`)
     .then(res=>res.json())
     .then(data=>setCustomer(data.result))
     
  }
-
     return (  
         <div>
           <button onClick={()=>handleoneuser(5)}> fetch</button>
           <button onClick={()=>console.log(customer)}> get</button>
           <h1>Customer Details</h1>  
-          <p>{customer.Username}</p>
-          <p>{customer.Email}</p>
-          <p>{customer.Phone}</p>
-          <p>{customer.address}</p>
-  
+          <p>{"name :"+customer.Username}</p>
+          <p>{"Email"+customer.Email}</p>
+          <p>{"Phone"+customer.Phone}</p>
+          <p>{"address"+customer.address}</p>
+          
           
         </div>
     );
