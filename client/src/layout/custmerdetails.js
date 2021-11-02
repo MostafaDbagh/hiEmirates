80ae3ae9da823418c00b3608d92a70ac5b6d35fe
MostafaDbagh <dabagmostafa@gmail.com>
2021-11-02 12:23:34 -0700

    make it better

--- client/src/layout/custmerdetails.js
+++ client/src/layout/custmerdetails.js
@@ -18,7 +18,9 @@ const Customerdetails  = () => {
           <p>{"Email"+customer.Email}</p>
           <p>{"Phone"+customer.Phone}</p>
           <p>{"address"+customer.address}</p>
-          
+          <img src={"../../../uploads/"+customer.Personalimage}></img>
+          <img src={}></img>
+          <img src={}></img>
           
         </div>
     );
