0856465ee52de1e185368073a75ef199f772bd38
XOiRD <a.f.bahemdan@gmail.com>
2021-11-04 11:22:10 +0400

    news page done

---
+++ client/src/style/news.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+
+
+export const  Div =styled.div`
+width:50vw;
+height:190px;
+background:rgb(16,30,40);
+text-aling:center;
+position:relative;
+margin:64px  auto 12px auto;
+border-radius:5px;
+color:white;
+box-shadow:0 0 2px 1px black;
+`
+export const Title = styled.h2`
+text-align:center;
+padding:12px ;
+margin:0;
+font-family: 'Almarai',sans-serif;
+color:rgb(191,214,48)
+`
+export const Newsp = styled.p`
+text-align:center;
+padding:6px ;
+margin:6px auto;
+font-family: 'El Messiri', sans-serif;
+width:88%;
+`
