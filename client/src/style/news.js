6b6d1648bf18efb82d892a9121f5eaef6a15a7b2
XOiRD <a.f.bahemdan@gmail.com>
2021-11-06 17:01:25 +0400

    about us page

--- client/src/style/news.js
+++ client/src/style/news.js
@@ -3,15 +3,18 @@ import styled from "styled-components";
 
 
 export const  Div =styled.div`
-width:50vw;
-height:190px;
+width:70vw;
+min-height:220px;
 background:rgb(16,30,40);
 text-aling:center;
 position:relative;
-margin:64px  auto 12px auto;
+margin:32px  auto 32px auto;
 border-radius:5px;
 color:white;
 box-shadow:0 0 2px 1px black;
+@media(max-width:670px){
+    width:94vw;
+}
 `
 export const Title = styled.h2`
 text-align:center;
