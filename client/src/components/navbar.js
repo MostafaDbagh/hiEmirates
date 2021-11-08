3a9720737c3f9848dd7f5969457d704aa9f92144
XOiRD <a.f.bahemdan@gmail.com>
2021-11-08 15:40:31 +0400

    deleivery page update

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
     <List><Anchor href="/Rent">أنواع السكن</Anchor></List>
     <List><Anchor href="/Services">خدمات أخرى</Anchor></List>
     <List><Anchor href="/Aboutus" > من نحن</Anchor></List>
-    <List><Anchor href="/Contact">اتصل بنا</Anchor></List>
+   
 
   
     </Unorderlist>
---
+++ client/src/style/service.js
@@ -0,0 +1,29 @@
+import styled  from "styled-components";
+
+
+export const Label = styled.label`
+font-family: 'Almarai', sans-serif;
+margin:0 24px 12px 0;
+`
+
+export const Input = styled.input`
+width:300px;
+height:40px;
+padding:8px 16px;
+margin:0 24px 12px 0;
+font-family: 'El Messiri', sans-serif;
+`
+export const Para = styled.p`
+font-family: 'Almarai', sans-serif;
+direction:rtl;
+padding:24px;
+margin-right:4px;
+font-weight:bold;
+`
+export const Select = styled.select`
+width:340px;
+height:60px;
+padding:8px 16px;
+margin:0 24px 12px 0;
+
+`
