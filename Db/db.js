3222a74f9c4c83a8146e387228c524dc72478ea6
MostafaDbagh <dabagmostafa@gmail.com>
2021-11-09 01:46:30 -0800

    checking for getuser image

--- Db/db.js
+++ Db/db.js
@@ -1,13 +1,18 @@
 var mysql = require('mysql');
 
 var db = mysql.createConnection({
-  host     : 'us-cdbr-east-04.cleardb.com',
-  user     : 'be6e9cc96c8b0a',
-  password : 'ab56e9e7',
-  database:"heroku_494961cf20d3161"
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database:"uaevisa"
  
 });
-
+db.connect(err=>{
+    if(err){
+       throw err
+    }
+    console.log('you are connected ')
+})
  
 module.exports =db; 
 
--- client/src/layout/news.js
+++ client/src/layout/news.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const News = () => {
+    const [post,setPost] = useState([])
+    function fetchPost() {
+        fetch(`http://localhost:4000/getAllposts`)
+        .then(res=>res.json())
+        .then(data=>setPost(data.result))
+        console.log(post)
+     }
+     useEffect(()=>{
+         fetchPost()
+     },[post])
     return ( 
-        
+        <>
         <h1>Last News</h1>
+        {post.map(i=>
+         <div>
+         <h2>{i.title}</h2>
+         <p>{i.post}</p>
+         </div>
+            )}
+       
+        </>
     );
 }
  
